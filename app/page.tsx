078706edd88f21f646e0af4432e79d1e72059732
oliveross123 <oliverstroch@seznam.cz>
2024-12-09 17:58:58 -0600

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1 className="h1-bold">Welcome to the world of next.js</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of next.js
+      </h1>
+    </>
+  );
 }
