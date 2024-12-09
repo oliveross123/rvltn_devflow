eff2fa85bbd7fe6dbf909fda10fe0fcf6335401e
oliveross123 <oliverstroch@seznam.cz>
2024-12-09 17:39:31 -0600

    Globals.css and tailwind.config changes for good workoflow in the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-black text-white">
-      Welcome to the world of next.js
-    </h1>
-  );
+  return <h1 className="h1-bold">Welcome to the world of next.js</h1>;
 }
