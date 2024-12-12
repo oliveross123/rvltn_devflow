aac435a0f0a92fffdef592616f9b49351b87f4a1
oliveross123 <oliverstroch@seznam.cz>
2024-12-11 21:43:49 -0600

    Setup auth Layouts, Social Auth FOrm, Next Auth, OAuth google and github provider

---
+++ constants/route.ts
@@ -0,0 +1,10 @@
+const ROUTES = {
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+};
+
+export default ROUTES;
+
+// Making this because of re-usability,
+// EXAMPLE: of usage :  redirect(ROUTES.SIGN_IN) less error prone
